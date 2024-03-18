4794ff25d02e49849af67d0f9267c7a0dfe662ad
one_punch_man <ckim@banlaw.com>
2024-03-18 22:47:59 +1100

    Units

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -2,21 +2,22 @@ import { FeedWrapper } from "@/components/feed-wrapper";
 import { Header } from "./header";
 import { StickyWrapper } from "@/components/sticky-wrapper";
 import { UserProgress } from "@/components/user-progress";
-import { getUserProgress } from "@/db/queries";
+import { getUnits, getUserProgress } from "@/db/queries";
 import { redirect } from "next/navigation";
 
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const unitData = getUnits();
 
-  const [userProgress] = await Promise.all([userProgressData]);
+  const [userProgress, units] = await Promise.all([userProgressData, unitData]);
 
   if (!userProgress || !userProgress.activeCourse) {
     redirect("/courses");
   }
 
-  if (!courseProgress) {
-    redirect("/courses");
-  }
+  // if (!courseProgress) {
+  //   redirect("/courses");
+  // }
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -30,6 +31,11 @@ const LearnPage = async () => {
       </StickyWrapper>
       <FeedWrapper>
         <Header title={userProgress.activeCourse.title} />
+        {units.map((unit) => (
+          <div key={unit.id} className="mb-10">
+            {JSON.stringify(unit)}
+          </div>
+        ))}
       </FeedWrapper>
     </div>
   );
--- db/queries.ts
+++ db/queries.ts
@@ -10,6 +10,7 @@ import {
   userSubscription,
 } from "@/db/schema";
 import { eq } from "drizzle-orm";
+import exp from "constants";
 
 export const getUserProgress = cache(async () => {
   const { userId } = await auth();
@@ -32,6 +33,57 @@ export const getCourses = cache(async () => {
   const data = await db.query.courses.findMany();
 
   return data;
+});
+
+export const getUnits = cache(async () => {
+  const { userId } = await auth();
+  const userProgress = await getUserProgress();
+
+  if (!userId || !userProgress?.activeCourseId) {
+    return [];
+  }
+
+  const data = await db.query.units.findMany({
+    orderBy: (units, { asc }) => [asc(units.order)],
+    where: eq(units.courseId, userProgress.activeCourseId),
+    with: {
+      lessons: {
+        orderBy: (lessons, { asc }) => [asc(lessons.order)],
+        with: {
+          challenges: {
+            orderBy: (challenges, { asc }) => [asc(challenges.order)],
+            with: {
+              challengeProgress: {
+                where: eq(challengeProgress.userId, userId),
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+
+  const normalizedData = data.map((unit) => {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      if (lesson.challenges.length === 0) {
+        return { ...lesson, completed: false };
+      }
+
+      const allCompletedChallenges = lesson.challenges.every((challenge) => {
+        return (
+          challenge.challengeProgress &&
+          challenge.challengeProgress.length > 0 &&
+          challenge.challengeProgress.every((progress) => progress.completed)
+        );
+      });
+
+      return { ...lesson, completed: allCompletedChallenges };
+    });
+
+    return { ...unit, lessons: lessonsWithCompletedStatus };
+  });
+
+  return normalizedData;
 });
 
 export const getCourseById = cache(async (courseId: number) => {
